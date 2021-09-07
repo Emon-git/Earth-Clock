bd1e47142eb761edf053339b166cf1a2941f7d96
Emon36666 <memon181132@bscse.uiu.ac.bd>
2021-09-08 02:12:20 +0600

    Modification for Text clock and dd/mm/yy

--- j_script.js
+++ j_script.js
@@ -19,3 +19,69 @@ seconds.style.transform = `rotateZ(${ss}deg)`
 }
 setInterval(clock,1000) // 1000=1s
 
+
+//Clock & Date
+
+const textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAMPM = document.getElementById('text-AMPM'),
+      dateDay = document.getElementById('date-day'),
+      dateMonth = document.getElementById('date-month'),
+      dateYear = document.getElementById('date-year')
+
+const clockText = () =>{
+    let date = new Date();
+
+
+    let hh = date.getHours(),
+        ampm,
+        mm = date.getMinutes(),
+        dat = date.getDate(),
+        mon = date.getMonth(),
+        yr = date.getFullYear();
+
+    if(hh >= 12){
+        hh = hh - 12;
+        ampm = 'PM';
+    }else{
+        ampm = 'AM';
+    }
+
+    if(hh == 0){
+        hh = 12;
+    }
+    
+
+
+    textHour.innerHTML = `${hh}:`
+    textMinutes.innerHTML = `${mm}`
+    textAMPM.innerHTML = `${ampm}`
+
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                  'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+
+    dateDay.innerHTML = `${dat}`
+    dateMonth.innerHTML = `${months[mon]}, `,
+    dateYear.innerHTML = yr
+
+    if(hh < 10){
+        textHour.innerHTML = `0${hh}:`
+    }
+
+    if(mm < 10){
+        textMinutes.innerHTML = `0${mm}`
+    }
+
+    /*if(mon < 10){
+        dateMonth.innerHTML = `0${mon}-`
+    }
+    
+    if(dat < 10){
+        dateDay.innerHTML = `0${dat}-`
+    }*/
+}
+
+setInterval(clockText,1000) // 1000=1s
+
